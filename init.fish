dad67795f9231b7db8b4a445ba63330392239b24
Jenn Wheeler <jawheeler@wm.edu>
2021-05-03 16:32:26 -0400

    fix path setting in init.fish

--- init.fish
+++ init.fish
@@ -2,19 +2,26 @@ if command -v pyenv 1>/dev/null 2>&1
     pyenv init - | source
 end
 
-set -Upx PATH "$HOME/.cargo/bin:$PATH"
-set -Upx PATH "$HOME/go/bin:$PATH"
-set -Upx PATH "$HOME/.gem/ruby/2.6.0/bin:$PATH"
-set -Upx PATH "$HOME/.local/bin:$PATH"
-set -Upx PATH "/usr/local/sbin:$PATH"
-set -Upx PATH "$HOME/bin:$PATH"
+set -px PATH $HOME/.cargo/bin
+set -px PATH $HOME/go/bin
+set -px PATH $HOME/.gem/ruby/2.6.0/bin
+set -px PATH $HOME/.local/bin
+set -px PATH /usr/local/sbin
+set -px PATH /usr/local/bin
+set -px PATH $HOME/bin
+set -px PATH /opt/homebrew/bin
+set -px PATH /opt/homebrew/opt/llvm/bin
+
+set -px PATH $HOME/.local/share/gem/ruby/2.7.0/bin
 
 set -Ux GPG_TTY `tty`
 
 set -Ux GO111MODULE on
 
 # use most pager for man pages
-set -Ux MANPAGER most
+if command -v pyenv 1>/dev/null 2>&1
+    set -Ux MANPAGER most
+end
 
 if command -v nvim 1>/dev/null 2>&1
     alias vim="nvim"
